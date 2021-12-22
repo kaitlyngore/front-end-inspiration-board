82ec8f2ba1c27454ae59a5ce89c0c3b0bfe022bd
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-12-22 13:26:49 -0600

    created Board function to display initial board and submit button to handle data

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,2 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './Form.css';
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Board Created')
+  }
+const Board = () => {
+    const [title, setTitle] = useState(null);
+    const [owner, setOwner] = useState(null);
+    return (
+      <div className="Board">
+        <h3>Create Board</h3>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <label>
+            <p>Title: </p>
+            <input type="text" name="title" />
+            </label>
+            <label>
+            <p> Owner: </p>
+            <input type="text" name="owner" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+        </form>
+          </div>
+    );
+  }
+
+
+export default Board;
